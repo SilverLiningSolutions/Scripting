fdc9f1c6ba4dc977ba75aba532dd05a14bf69e2f
KingsRiver <jason@kingsriversolutions.com>
2017-03-10 15:36:58 -0800

    Add files via upload

---
+++ Functions/Record Functions/addParent.js
@@ -0,0 +1,19 @@
+function addParent(parentAppNum) 
+//
+// adds the current application to the parent
+//
+	{
+	var getCapResult = aa.cap.getCapID(parentAppNum);
+	if (getCapResult.getSuccess())
+		{
+		var parentId = getCapResult.getOutput();
+		var linkResult = aa.cap.createAppHierarchy(parentId, capId);
+		if (linkResult.getSuccess())
+			logDebug("Successfully linked to Parent Application : " + parentAppNum);
+		else
+			logDebug( "**ERROR: linking to parent application parent cap id (" + parentAppNum + "): " + linkResult.getErrorMessage());
+		}
+	else
+		{ logDebug( "**ERROR: getting parent cap id (" + parentAppNum + "): " + getCapResult.getErrorMessage()) }
+	}
+	
