029d6e7e9c1ea66d7b1127c3867ac27dcd40f5f9
KingsRiver <jason@kingsriversolutions.com>
2017-03-10 15:42:39 -0800

    Add files via upload

---
+++ Functions/Utility Functions/xmlExampleXMLToHTML.js
@@ -0,0 +1,59 @@
+function xmlEscapeXMLToHTML(xmlData) {
+    /*************************************************************************************
+    Function:       xmlEscapeXMLToHTML
+
+    author:         xwisdom@yahoo.com
+
+    description:
+        Encodes XML data for use in a web page
+
+    ************************************************************************************/
+    var gt;
+
+    var str = xmlData;
+
+    //replace & with &
+    gt = -1;
+    while (str.indexOf("&", gt + 1) > -1) {
+        var gt = str.indexOf("&", gt + 1);
+        var newStr = str.substr(0, gt);
+        newStr += "&";
+        newStr = newStr + str.substr(gt + 1, str.length);
+        str = newStr;
+    }
+
+    //replace < with <
+    gt = -1;
+    while (str.indexOf("<", gt + 1) > -1) {
+        var gt = str.indexOf("<", gt + 1);
+        var newStr = str.substr(0, gt);
+        newStr += "<";
+        newStr = newStr + str.substr(gt + 1, str.length);
+        str = newStr;
+    }
+
+    //replace > with >
+    gt = -1;
+    while (str.indexOf(">", gt + 1) > -1) {
+        var gt = str.indexOf(">", gt + 1);
+        var newStr = str.substr(0, gt);
+        newStr += ">";
+        newStr = newStr + str.substr(gt + 1, str.length);
+        str = newStr;
+    }
+
+    //replace \n with <br>
+    gt = -1;
+    while (str.indexOf("\n", gt + 1) > -1) {
+        var gt = str.indexOf("\n", gt + 1);
+        var newStr = str.substr(0, gt);
+        newStr += "<br>";
+        newStr = newStr + str.substr(gt + 1, str.length);
+        str = newStr;
+    }
+
+    return str
+
+}  // end function xmlEscapeXMLToHTML
+
+
---
+++ Functions/Workflow Functions/Task.js
@@ -0,0 +1,14 @@
+
+function Task() // Task Object
+	{
+	this.status = null
+	this.comment = null;
+	this.note = null;
+    this.statusdate = null;
+	this.process = null;
+	this.processID = null;
+    this.step = null;
+    this.active = null;
+	}
+	
+
